c7cf401d792293c566ed41bb78152eed569fa6cb
Aldric-L <al9.labarthe@free.fr>
2024-01-26 12:44:49 +0100

    Initial Commit

---
+++ CournotDDPG/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CournotDDPG
+//
+//  Created by Aldric Labarthe on 26/01/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
