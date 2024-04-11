92de85f3fb730277723fc932a7329c2fccec8ea4
Aldric-L <al9.labarthe@free.fr>
2024-04-11 19:53:23 +0200

    A new batch of simulations for collusion

--- DataProcessing/Outputs/Cournot-2-90k-TRUNC_EXP/launch.sh
+++ DataProcessing/Outputs/Cournot-2-90k-TRUNC_EXP/launch.sh
@@ -1,7 +1,1 @@
-./../../../build/DDPG-Oligopolies-Simulator -S=33 -n=2 -N=60000 -g=0 -m=500 -f=1 -w=TRUNC_EXP
-./../../../build/DDPG-Oligopolies-Simulator -S=12 -n=2 -N=90000 -g=0 -m=1000 -f=1 -d=0.9999 -w=TRUNC_EXP
-./../../../build/DDPG-Oligopolies-Simulator -S=12 -n=2 -N=90000 -g=0 -m=1000 -f=1 -d=0.9999 -w=TRUNC_EXP
-./../../../build/DDPG-Oligopolies-Simulator -S=12 -n=2 -N=90000 -g=0.5 -m=1000 -f=1 -d=0.9999 -w=TRUNC_EXP
-./../../../build/DDPG-Oligopolies-Simulator -S=12 -n=2 -N=90000 -g=0.3 -m=1000 -f=1 -d=0.9999 -w=TRUNC_EXP
-./../../../build/DDPG-Oligopolies-Simulator -S=12 -n=2 -N=90000 -g=0.7 -m=1000 -f=1 -d=0.9999 -w=TRUNC_EXP
-./../../../build/DDPG-Oligopolies-Simulator -S=12 -n=2 -N=90000 -g=0.8 -m=1000 -f=1 -d=0.9999 -w=TRUNC_EXP
+./../../../build/DDPG-Oligopolies-Simulator -S=24 -n=2 -N=90000 -g=0.2 -m=1000 -f=1 -d=0.9999 -w=TRUNC_EXP
