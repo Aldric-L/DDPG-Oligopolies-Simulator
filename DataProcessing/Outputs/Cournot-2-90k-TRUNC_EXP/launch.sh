c2cb65507dc09168c34a16f1890a574e6a7e1bcc
Aldric-L <al9.labarthe@free.fr>
2024-04-12 07:46:56 +0200

    New batch of simulation for collusion

--- DataProcessing/Outputs/Cournot-2-90k-TRUNC_EXP/launch.sh
+++ DataProcessing/Outputs/Cournot-2-90k-TRUNC_EXP/launch.sh
@@ -1,1 +1,2 @@
-./../../../build/DDPG-Oligopolies-Simulator -S=24 -n=2 -N=90000 -g=0.2 -m=1000 -f=1 -d=0.9999 -w=TRUNC_EXP
+./../../../build/DDPG-Oligopolies-Simulator -S=90 -n=2 -N=60000 -g=0 -m=1000 -f=1 -w=TRUNC_EXP
+./../../../build/DDPG-Oligopolies-Simulator -S=12 -n=2 -N=90000 -g=0.4 -m=1000 -f=1 -d=0.9999 -w=TRUNC_EXP
