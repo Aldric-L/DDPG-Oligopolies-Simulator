c5bb3c44db05e2bb51f0ff53a03a0ae120ee2dc5
Aldric-L <al9.labarthe@free.fr>
2024-04-15 10:10:47 +0200

    New batch of simulations in cournot4

--- DataProcessing/Outputs/Experimental/E58 (Cournot4)/launch.sh
+++ DataProcessing/Outputs/Experimental/E58 (Cournot4)/launch.sh
@@ -1,16 +1,10 @@
-./../../../../build/DDPG-Oligopolies-Simulator -S=15 -n=4 -N=90000 -g=0 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=15 -n=4 -N=90000 -g=0 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=15 -n=4 -N=90000 -g=0.5 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=25 -n=4 -N=90000 -g=0 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=25 -n=4 -N=90000 -g=0 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=25 -n=4 -N=90000 -g=0.5 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0.5 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0.5 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0.5 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
+./../../../../build/DDPG-Oligopolies-Simulator -S=75 -n=4 -N=90000 -g=0.5 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
+./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
+./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
+./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0.5 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
+./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0.5 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
+./../../../../build/DDPG-Oligopolies-Simulator -S=45 -n=4 -N=90000 -g=0 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
+./../../../../build/DDPG-Oligopolies-Simulator -S=45 -n=4 -N=90000 -g=0 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
+./../../../../build/DDPG-Oligopolies-Simulator -S=45 -n=4 -N=90000 -g=0.5 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
+./../../../../build/DDPG-Oligopolies-Simulator -S=45 -n=4 -N=90000 -g=0.5 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
 
