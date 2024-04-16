1577cc8ccc6a8e6dec787a0ae07c935731087cca
Aldric-L <al9.labarthe@free.fr>
2024-04-16 15:30:46 +0200

    New batch of simulations to assess memory effect

--- DataProcessing/Outputs/Experimental/E58 (Cournot4)/launch.sh
+++ DataProcessing/Outputs/Experimental/E58 (Cournot4)/launch.sh
@@ -1,10 +1,6 @@
-./../../../../build/DDPG-Oligopolies-Simulator -S=75 -n=4 -N=90000 -g=0.5 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0.5 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0.5 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=45 -n=4 -N=90000 -g=0 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=45 -n=4 -N=90000 -g=0 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=45 -n=4 -N=90000 -g=0.5 -m=500 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
-./../../../../build/DDPG-Oligopolies-Simulator -S=45 -n=4 -N=90000 -g=0.5 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
+#./../../../../build/DDPG-Oligopolies-Simulator -S=75 -n=4 -N=90000 -g=0.5 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
+#./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.5 -D=5 
+#./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0 -m=1000 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5 
+./../../../../build/DDPG-Oligopolies-Simulator -S=30 -n=4 -N=90000 -g=0 -m=2000 -f=0.25 -F=0.15 -d=0.9999 -C=0.6 -D=5
+
 
